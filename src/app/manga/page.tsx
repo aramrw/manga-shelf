81a73dbeacec4bb4eaf58ea02e4707e89611f0d8
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-17 01:48:00 -0700

    add manga main panels page

--- src/app/manga/page.tsx
+++ src/app/manga/page.tsx
@@ -1,7 +1,82 @@
+"use client";
+
+import { ParentFolderType } from "../dashboard/page";
+import fetchGlobalManga from "./_components/lib/fetch-global-manga";
+import MangaHeader from "./_components/manga-header";
+import { useEffect, useState } from "react";
+import { FileEntry, readDir } from "@tauri-apps/api/fs";
+import MangaPanel from "./_components/manga-panel";
+import { invoke } from "@tauri-apps/api/tauri";
+
 export default function Manga() {
-	return (
-		<main className="w-full h-full flex flex-col justify-center items-center bg-secondary">
-			<h1 className="font-bold">Manga</h1>
-		</main>
-	);
+  const [currentManga, setCurrentManga] = useState<ParentFolderType | null>(
+    null,
+  );
+  const [mangaPanels, setMangaPanels] = useState<FileEntry[]>([]);
+  const [currentPanelIndex, setCurrentPanelIndex] = useState<number>(0);
+
+  useEffect(() => {
+    fetchGlobalManga().then((manga) => {
+      if (manga) {
+        //console.log(manga);
+        setCurrentManga(manga);
+        readDir(manga.full_path).then((result) => {
+          if (result) {
+            setMangaPanels(result);
+          }
+        });
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (currentManga) invokeFindLastReadPanel();
+  }, [currentManga]);
+
+  const invokeFindLastReadPanel = async () => {
+    if (currentManga) {
+      invoke("find_last_read_panel", {
+        chapterPath: currentManga.full_path,
+      }).then((lastReadPanelIndex: unknown) => {
+        setCurrentPanelIndex(lastReadPanelIndex as number);
+      });
+    }
+  };
+
+  const handleNextPanel = () => {
+    if (currentPanelIndex < mangaPanels.length - 1) {
+      invoke("update_manga_panel", {
+        dirPath: mangaPanels[currentPanelIndex + 1].path,
+        isRead: true,
+      });
+      setCurrentPanelIndex((prev) => prev + 1);
+    }
+  };
+
+  const handlePreviousPanel = () => {
+    if (currentPanelIndex > 0) {
+      invoke("update_manga_panel", {
+        dirPath: mangaPanels[currentPanelIndex].path,
+        isRead: false,
+      });
+      setCurrentPanelIndex((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <main className="w-full h-full flex flex-col justify-center items-center">
+      {currentManga && (
+        <MangaHeader
+          currentManga={currentManga}
+          handleNextPanel={handleNextPanel}
+          handlePreviousPanel={handlePreviousPanel}
+        />
+      )}
+      <div>
+        {mangaPanels[currentPanelIndex] && (
+          <MangaPanel currentPanel={mangaPanels[currentPanelIndex]} />
+        )}
+      </div>
+    </main>
+  );
 }
