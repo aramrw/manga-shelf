346a6170f8f1ca44e2c343e1464bb76b3d2fa90b
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-17 02:39:00 -0700

    update navbar
    + dynamic padding
    + go back button

--- src/app/_components/nav/nav-bar.tsx
+++ src/app/_components/nav/nav-bar.tsx
@@ -4,30 +4,40 @@ import Link from "next/link";
 import { ChartBarIcon, CogIcon } from "@heroicons/react/16/solid";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { MoveLeft } from "lucide-react";
 
 export default function NavBar() {
   const router = useRouter();
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	useEffect(() => {
-		if (pathname == "/") {
-			router.push("/dashboard");
-		}
-			
-		}, [pathname]);
+  useEffect(() => {
+    if (pathname == "/") {
+      router.push("/dashboard");
+    }
+  }, [pathname]);
+
+  const handleGoBack = () => {
+    router.back();
+  };
 
   return (
-    <menu className="w-full h-8 bg-primary px-1">
+    <menu className="bg-primary px-1.5 md:px-30 z-50 top-0 sticky flex h-8 w-full flex-row items-center justify-between border-b-2 shadow-sm md:h-9 lg:h-10 lg:px-16 xl:px-36 2xl:px-48 ">
       <ul className="w-full h-full flex flex-row justify-between items-center">
+        <li
+          onClick={handleGoBack}
+          className="flex flex-row justify-center items-center cursor-pointer hover:opacity-80 rounded-sm text-accent"
+        >
+          <MoveLeft className="w-6 h-6" />
+        </li>
         <ul className="flex flex-row gap-1">
-          <li>
+          <li className="flex flex-row justify-center items-center cursor-pointer hover:opacity-80 rounded-sm text-accent">
             <Link href="/">
-              <ChartBarIcon className="w-6 h-6 text-white" />
+              <ChartBarIcon className="w-6 h-6" />
             </Link>
           </li>
-          <li>
+          <li className="flex flex-row justify-center items-center cursor-pointer hover:opacity-80 rounded-sm text-accent">
             <Link href="/">
-              <CogIcon className="w-6 h-6 text-white" />
+              <CogIcon className="w-6 h-6" />
             </Link>
           </li>
         </ul>
