133624d2826287e5eef5b3985a22657dc2522bfe
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-17 02:57:07 -0700

    update child folder no wrap

--- src/app/dashboard/_components/parent-folder.tsx
+++ src/app/dashboard/_components/parent-folder.tsx
@@ -105,7 +105,7 @@ export default function ParentFolder({
             {mangaFolders.map((mangaFolder, index) => (
               <h1
                 key={index}
-                className={cn("py-2 text-xs font-bold border-t-2 border-primary hover:opacity-70 transition-opacity duration-100",
+                className={cn("py-2 px-1 text-xs font-bold border-t-2 border-primary hover:opacity-70 transition-opacity duration-100 text-nowrap overflow-hidden overflow-ellipsis w-full",
 								index === 0 && "border-t-0" 
 								)}
                 onClick={() => handleMangaClick(mangaFolder.path)}
