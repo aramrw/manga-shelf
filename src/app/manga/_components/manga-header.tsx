f8646185ed6eaecee4d1f7cdc7e661d1b4b3ce0d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-17 01:47:31 -0700

    add manga panel header + navigation

---
+++ src/app/manga/_components/manga-header.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { ParentFolderType } from "@/app/dashboard/page";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useEffect } from "react";
+
+const MangaHeader = ({
+  currentManga,
+  handleNextPanel,
+  handlePreviousPanel,
+}: {
+  currentManga: ParentFolderType;
+  handleNextPanel: () => void;
+  handlePreviousPanel: () => void;
+}) => {
+
+	useEffect(() => {
+
+		function handleKeyDown(event: KeyboardEvent) {
+			if (event.key === "ArrowRight") {
+				handleNextPanel();
+			}
+			if (event.key === "ArrowLeft") {
+				handlePreviousPanel();
+			}
+		}
+
+		addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			removeEventListener("keydown", handleKeyDown);
+		};
+
+	}, [handleNextPanel, handlePreviousPanel]);
+
+  return (
+    <header className="w-full h-fit bg-secondary p-1.5 rounded-b-sm flex justify-center items-center">
+      <menu className="w-full h-full flex flex-row justify-center items-center">
+        <ul className="w-full h-full flex flex-row justify-center items-center gap-2">
+          <li className="flex flex-row justify-center items-center">
+            <Button className="p-0" onClick={handlePreviousPanel}>
+              <ChevronLeft size={20} />
+            </Button>
+          </li>
+          <li className="flex flex-row justify-center items-center select-none">
+            <h1 className="font-bold text-sm">{currentManga.title}</h1>
+          </li>
+          <li className="flex flex-row justify-center items-center">
+            <Button className="p-0" onClick={handleNextPanel}>
+              <ChevronRight size={20}/>
+            </Button>
+          </li>
+        </ul>
+      </menu>
+    </header>
+  );
+}
+
+export default MangaHeader;
