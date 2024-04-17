a2d092a178aead532e32377b13b45b75868c09d2
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-17 02:43:46 -0700

    fix nav height + padding

--- src/app/_components/nav/nav-bar.tsx
+++ src/app/_components/nav/nav-bar.tsx
@@ -21,23 +21,23 @@ export default function NavBar() {
   };
 
   return (
-    <menu className="bg-primary px-1.5 md:px-30 z-50 top-0 sticky flex h-8 w-full flex-row items-center justify-between border-b-2 shadow-sm md:h-9 lg:h-10 lg:px-16 xl:px-36 2xl:px-48 ">
+    <menu className="bg-primary px-1.5 md:px-30 z-50 top-0 sticky flex h-8 w-full flex-row items-center justify-between border-b-2 shadow-sm lg:px-16 xl:px-36 2xl:px-48 ">
       <ul className="w-full h-full flex flex-row justify-between items-center">
         <li
           onClick={handleGoBack}
           className="flex flex-row justify-center items-center cursor-pointer hover:opacity-80 rounded-sm text-accent"
         >
-          <MoveLeft className="w-6 h-6" />
+          <MoveLeft className="w-7 h-auto" />
         </li>
         <ul className="flex flex-row gap-1">
           <li className="flex flex-row justify-center items-center cursor-pointer hover:opacity-80 rounded-sm text-accent">
             <Link href="/">
-              <ChartBarIcon className="w-6 h-6" />
+              <ChartBarIcon className="w-6 h-auto" />
             </Link>
           </li>
           <li className="flex flex-row justify-center items-center cursor-pointer hover:opacity-80 rounded-sm text-accent">
             <Link href="/">
-              <CogIcon className="w-6 h-6" />
+              <CogIcon className="w-6 h-auto" />
             </Link>
           </li>
         </ul>
