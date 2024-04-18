639e59c055dc54b239aa17bb99c5cff4d06b8afc
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-18 00:58:45 -0700

    cleanup parent folder + context removal from page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,21 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { PlusCircleIcon, TrashIcon } from "@heroicons/react/16/solid";
+import { PlusCircleIcon } from "@heroicons/react/16/solid";
 import { open } from "@tauri-apps/api/dialog";
 import { invoke } from "@tauri-apps/api/tauri";
 import React, { useState, useEffect } from "react";
-import ParentFolder from "./_components/parent-folder";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuTrigger,
-} from "@/components/ui/context-menu";
+import ParentFolderContextMenu from "./_components/parent_folder/parent-folder-context-menu";
 
 export type ParentFolderType = {
   id: string;
   title: string;
   full_path: string;
   as_child: boolean;
-	is_expanded: boolean;
+  is_expanded: boolean;
   created_at: string;
   updated_at: string;
 };
-
 
 export default function Dashboard() {
   const [parentFolders, setParentFolders] = useState<ParentFolderType[]>([]);
@@ -44,19 +37,13 @@ export default function Dashboard() {
     invoke("update_manga_folders", {
       dirPaths: JSON.stringify(dirs),
       asChild: false,
-			isExpanded: false,
+      isExpanded: false,
     }).then((result: unknown) => {
       if (result) {
         const currentParentFolders: ParentFolderType[] =
           result as ParentFolderType[];
         setParentFolders((prev) => [...prev, ...currentParentFolders]);
       }
-    });
-  };
-
-  const handleInvokeDeleteParentFolder = (id: string, path: string) => {
-    invoke("delete_manga_folder", { id, path }).then(() => {
-      setParentFolders((prev) => prev.filter((f) => f.id !== id));
     });
   };
 
@@ -88,20 +75,11 @@ export default function Dashboard() {
         <ul className="w-full h-fit grid grid-cols-4 gap-2 mt-4">
           {parentFolders.map((folder, index) => {
             return (
-              <ContextMenu key={index}>
-                <ContextMenuTrigger>
-                  <ParentFolder key={index} parentFolder={folder} />
-                </ContextMenuTrigger>
-                <ContextMenuContent>
-                  <ContextMenuItem
-                    className="font-semibold flex flex-row items-center gap-0.5 cursor-pointer"
-                    onClick={() => handleInvokeDeleteParentFolder(folder.id, folder.full_path)}
-                  >
-                    <span>Delete</span>
-                    <TrashIcon className="h-4 w-auto" />
-                  </ContextMenuItem>
-                </ContextMenuContent>
-              </ContextMenu>
+              <ParentFolderContextMenu
+                key={index}
+                folder={folder}
+                setParentFolders={setParentFolders}
+              />
             );
           })}
         </ul>
