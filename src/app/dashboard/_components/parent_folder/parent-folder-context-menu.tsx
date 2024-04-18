f8337cc61dedd4e765f811c83c88fafccae846a3
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-18 00:58:17 -0700

    refactor context menu into component outside of page.tsx

---
+++ src/app/dashboard/_components/parent_folder/parent-folder-context-menu.tsx
@@ -0,0 +1,58 @@
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import { ParentFolderType } from "../../page";
+import { FolderIcon, TrashIcon } from "@heroicons/react/16/solid";
+import { invoke } from "@tauri-apps/api/tauri";
+import ParentFolder from "./parent-folder";
+
+export default function ParentFolderContextMenu({
+  folder,
+  setParentFolders,
+}: {
+  folder: ParentFolderType;
+  setParentFolders: React.Dispatch<React.SetStateAction<ParentFolderType[]>>;
+}) {
+  const handleInvokeDeleteParentFolder = (id: string, path: string) => {
+    invoke("delete_manga_folder", { id, path }).then(() => {
+      setParentFolders((prev) => prev.filter((f) => f.id !== id));
+    });
+  };
+
+	const handleInvokeShowInFolder = (path: string) => {
+		invoke("show_in_folder", { path });	
+	};
+
+  return (
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <ParentFolder parentFolder={folder} />
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem
+          className="font-semibold flex flex-row items-center gap-0.5 cursor-pointer"
+          onClick={() =>
+						handleInvokeShowInFolder(folder.full_path)
+          }
+        >
+          <span>Show In Folder</span>
+          <FolderIcon className="h-4 w-auto" />
+        </ContextMenuItem>
+        <ContextMenuSeparator />
+        <ContextMenuItem
+          className="font-semibold flex flex-row items-center gap-0.5 cursor-pointer"
+          onClick={() =>
+            handleInvokeDeleteParentFolder(folder.id, folder.full_path)
+          }
+        >
+          <span>Delete</span>
+          <TrashIcon className="h-4 w-auto" />
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
+  );
+}
