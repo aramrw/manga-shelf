d42e152218391306e88a4997681395188070f00d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-18 17:41:08 -0700

    add magnify + minify buttons to manga header

--- src/app/manga/_components/manga-header.tsx
+++ src/app/manga/_components/manga-header.tsx
@@ -2,40 +2,58 @@
 
 import { ParentFolderType } from "@/app/dashboard/page";
 import { Button } from "@/components/ui/button";
+import {
+  MagnifyingGlassMinusIcon,
+  MagnifyingGlassPlusIcon,
+} from "@heroicons/react/16/solid";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useEffect } from "react";
 
 const MangaHeader = ({
   currentManga,
+  largePanel,
+	setZoomLevel,
   handleNextPanel,
   handleNextSinglePanel,
   handlePreviousPanel,
-	handlePreviousSinglePanel,
+  handlePreviousSinglePanel,
 }: {
   currentManga: ParentFolderType;
+  largePanel: boolean;
+	setZoomLevel: React.Dispatch<React.SetStateAction<number>>;
   handleNextPanel: () => void;
   handleNextSinglePanel: () => void;
   handlePreviousPanel: () => void;
-	handlePreviousSinglePanel: () => void;
+  handlePreviousSinglePanel: () => void;
 }) => {
+
+
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
-			// previous panel
+      // previous panel
       if (event.shiftKey && event.key === "ArrowRight") {
         handlePreviousSinglePanel();
       } else if (event.altKey && event.key === "ArrowRight") {
         // handleNextPanel();
       } else if (event.key === "ArrowRight") {
-        handlePreviousPanel();
+        if (largePanel) {
+          handlePreviousSinglePanel();
+        } else {
+          handlePreviousPanel();
+        }
       }
 
       // next panel
-       if (event.shiftKey && event.key === "ArrowLeft") {
+      if (event.shiftKey && event.key === "ArrowLeft") {
         handleNextSinglePanel();
       } else if (event.altKey && event.key === "ArrowLeft") {
         // handleNextPanel();
       } else if (event.key === "ArrowLeft") {
-        handleNextPanel();
+        if (largePanel) {
+          handleNextSinglePanel();
+        } else {
+          handleNextPanel();
+        }
       }
     }
 
@@ -46,9 +64,26 @@ const MangaHeader = ({
     };
   }, [handleNextPanel, handlePreviousPanel]);
 
+
   return (
     <header className="w-full h-fit bg-secondary p-1.5 rounded-b-sm flex justify-center items-center">
       <menu className="w-full h-full flex flex-row justify-center items-center">
+        <ul className="w-fit h-fit flex flex-row justify-center items-center gap-1">
+          <li className="flex flex-row justify-center items-center">
+            <Button className="py-0.5 px-1"
+							onClick={() => setZoomLevel((prev) => prev + 10)}
+							>
+              <MagnifyingGlassPlusIcon className="w-4 h-auto" />
+            </Button>
+          </li>
+          <li className="flex flex-row justify-center items-center">
+            <Button className="py-0.5 px-1"
+							onClick={() => setZoomLevel((prev) => prev - 10)}
+							>
+              <MagnifyingGlassMinusIcon className="w-4 h-auto" />
+            </Button>
+          </li>
+        </ul>
         <ul className="w-full h-full flex flex-row justify-center items-center gap-2">
           <li className="flex flex-row justify-center items-center">
             <Button className="p-0" onClick={handleNextPanel}>
