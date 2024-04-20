a9ebdba899befa1450eb3e926c07bae32b7b125b
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-19 20:08:22 -0700

    stats struct

--- src-tauri/src/stats.rs
+++ src-tauri/src/stats.rs
@@ -5,3 +5,11 @@ use sqlx::{Row, SqlitePool};
 use tauri::{AppHandle, Manager};
 use tokio::sync::Mutex;
 use crate::manga::{MangaFolder, MangaPanel};
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Stats {
+    pub total_manga: i32,
+    pub total_panels: i32,
+    pub total_panels_read: i32,
+    pub total_panels_remaining: i32,
+}
