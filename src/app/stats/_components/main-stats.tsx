36b2986b4a16bde0b8a7acf0b2798f3da5538217
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-20 14:24:59 -0700

    main stats render total time spent reading

--- src/app/stats/_components/main-stats.tsx
+++ src/app/stats/_components/main-stats.tsx
@@ -9,6 +9,7 @@ export type MainStatsType = {
   total_panels: number;
   total_panels_read: number;
   total_panels_remaining: number;
+	total_time_spent_reading: number;
 };
 
 export default function MainStats() {
@@ -22,10 +23,24 @@ export default function MainStats() {
     });
   }, []);
 
+	function calculateTimeSpentReading(time: number | undefined) {
+		// if time (seconds) is greater than a minute render both minutes and seconds
+		// else render only seconds
+		if (time && time > 60) {
+			const minutes = time / 60;
+			const seconds = time % 60;
+			return `${minutes}m ${seconds}s`;
+		} else if (time) {
+			return `${time}s`;
+		} else {
+			return "0s";
+		}
+	}
+
   return (
     <div className="w-full flex flex-col justify-center items-center h-fit bg-card rounded-xl shadow-md outline outline-border p-2">
       <h1 className="font-bold rounded-md px-1">Main Stats</h1>
-      <Separator className="h-[1.1px] w-[23.7rem] mb-1.5 mr-0.5" />
+      <Separator className="h-[1.1px] w-1/5 mb-1.5 mr-0.5" />
       <ul className="flex flex-row justify-center items-start gap-3 pb-0.5">
         <li className="flex flex-col justify-center items-start gap-0.5 text-xs">
           <label className="font-semibold underline">Total Manga</label>
@@ -52,6 +67,12 @@ export default function MainStats() {
           <label className="font-semibold underline">Panels Remaining</label>
           <span className="font-medium bg-accent-foreground rounded-sm px-0.5">
             {mainStats?.total_panels_remaining}
+          </span>
+        </li>
+        <li className="flex flex-col justify-center items-start gap-0.5 text-xs">
+          <label className="font-semibold underline">Time Spent Reading</label>
+          <span className="font-medium bg-accent-foreground rounded-sm px-0.5">
+						{calculateTimeSpentReading(mainStats?.total_time_spent_reading)}
           </span>
         </li>
       </ul>
