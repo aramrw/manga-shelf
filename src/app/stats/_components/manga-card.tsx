3e620d434d5c183a5b8f28548beae0395418a9a5
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-20 14:24:47 -0700

    manga card displays time spent reading

--- src/app/stats/_components/manga-card.tsx
+++ src/app/stats/_components/manga-card.tsx
@@ -1,4 +1,4 @@
-import { ParentFolderType } from "@/app/dashboard/page";
+import { MangaFolderType } from "@/app/dashboard/page";
 import {
   HoverCard,
   HoverCardContent,
@@ -8,8 +8,22 @@ import {
 export default function MangaCard({
   mangaFolder,
 }: {
-  mangaFolder: ParentFolderType;
+  mangaFolder: MangaFolderType;
 }) {
+  function calculateTimeSpentReading(time: number | undefined) {
+    // if time (seconds) is greater than a minute render both minutes and seconds
+    // else render only seconds
+    if (time && time > 60) {
+      const minutes = time / 60;
+      const seconds = time % 60;
+      return `${minutes}m ${seconds}s`;
+    } else if (time) {
+      return `${time}s`;
+    } else {
+      return "0s";
+    }
+  }
+
   return (
     <div className="px-1 h-fit shadow-sm w-full">
       <HoverCard>
@@ -18,27 +32,37 @@ export default function MangaCard({
             {mangaFolder.title}
           </h2>
         </HoverCardTrigger>
-        <HoverCardContent className="w-fit px-1 flex flex-row justify-center items-center pt-0 pb-1.5">
+        <HoverCardContent className="w-fit px-1 flex flex-row justify-center items-center pt-0.5 pb-1.5">
           <ul className="flex flex-col justify-center items-center gap-0.5">
-						<li className="text-center flex underline flex-col justify-center items-center gap-0.5 text-[11px] font-bold">
-							<span className="bg-muted rounded-sm px-0.5">
-								{mangaFolder.title}
-							</span>
-						</li>
+            <li className="text-center flex underline flex-col justify-center items-center gap-0.5 text-[11px] font-bold">
+              <span className="bg-muted rounded-sm px-0.5">
+                {mangaFolder.title}
+              </span>
+            </li>
             <ul className="flex flex-row justify-center items-center gap-2">
-              <li className="flex flex-col justify-center items-center text-[9px]">
-                <label className="font-semibold">Updated</label>
-                <span className="font-medium bg-muted rounded-sm px-0.5">
+              <li className="flex flex-col justify-center items-center ">
+                <label className="font-semibold text-[10px]">Updated</label>
+                <span className="font-medium bg-accent-foreground rounded-sm px-0.5 text-[7px]">
                   {mangaFolder.updated_at}
                 </span>
               </li>
-              <li className="flex flex-col justify-center items-center text-[9px]">
-                <label className="font-semibold">Created</label>
-                <span className="font-medium bg-muted rounded-sm px-0.5">
+              <li className="flex flex-col justify-center items-center">
+                <label className="font-semibold text-[10px]">Created</label>
+                <span className="font-medium bg-accent-foreground rounded-sm px-0.5 text-[7px]">
                   {mangaFolder.created_at}
                 </span>
               </li>
             </ul>
+            <li className="text-center flex flex-col justify-center items-center">
+              <label className="font-semibold text-[10px]">
+                Time Spent Reading
+              </label>
+              <span className="bg-accent-foreground rounded-sm px-0.5 font-medium text-[9px]">
+                {calculateTimeSpentReading(
+                  mangaFolder.time_spent_reading,
+                )}
+              </span>
+            </li>
           </ul>
         </HoverCardContent>
       </HoverCard>
