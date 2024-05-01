b829c5c1a03cd3b17574d4f58a3f58c2802dd8e3
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-01 11:50:35 -0700

    format

--- src/app/manga/_components/manga-panel.tsx
+++ src/app/manga/_components/manga-panel.tsx
@@ -16,47 +16,42 @@ export default function MangaPanel({
   width: number;
   height: number;
 }) {
-
- 
-return (
-  <div className="w-full h-full flex flex-col justify-center items-center pt-6">
-    {currentPanel && currentPanel.name && (
-      <>
-        {secondPanel ? (
-          <h1 className="font-bold fixed left-2 bottom-1 text-xs text-zinc-300">
-            {currentPanel.name}
-          </h1>
-        ) : (
-          <h1 className="font-bold fixed right-2 bottom-1 text-xs text-zinc-300">
-            {currentPanel.name}
-          </h1>
-        )}
-        <div
-          id="IMAGE-DIV"
-          style={{
-            height: `${zoomLevel}px`,
-            width: "auto",
-          }}
-        >
-          <Image
-            src={convertFileSrc(currentPanel.path)}
-            alt={currentPanel.name}
-            width={width}
-            height={height}
-            quality={100}
-            priority={true}
-            className={cn(
-              "w-full h-full",
-              !secondPanel &&
-                "shadow-[10px_0_20px_-10px_rgba(0,0,0,0.45)]",
-              secondPanel &&
-                "shadow-[-10px_0_20px_-10px_rgba(0,0,0,0.45)]",
-            )}
-          />
-        </div>
-      </>
-    )}
-  </div>
-);
-
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center pt-6">
+      {currentPanel && currentPanel.name && (
+        <>
+          {secondPanel ? (
+            <h2 className="font-bold fixed left-2 bottom-1 text-xs text-zinc-300">
+              {currentPanel.name}
+            </h2>
+          ) : (
+            <h2 className="font-bold fixed right-2 bottom-1 text-xs text-zinc-300">
+              {currentPanel.name}
+            </h2>
+          )}
+          <div
+            id="IMAGE-DIV"
+            style={{
+              height: `${zoomLevel}px`,
+              width: "auto",
+            }}
+          >
+            <Image
+              src={convertFileSrc(currentPanel.path)}
+              alt={currentPanel.name}
+              width={width}
+              height={height}
+              quality={100}
+              priority={true}
+              className={cn(
+                "w-full h-full",
+                !secondPanel && "shadow-[10px_0_20px_-10px_rgba(0,0,0,0.45)]",
+                secondPanel && "shadow-[-10px_0_20px_-10px_rgba(0,0,0,0.45)]",
+              )}
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
