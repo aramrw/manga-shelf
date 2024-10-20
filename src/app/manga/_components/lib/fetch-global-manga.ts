6326bbb3b59ef36b96154c9c721090ca029964c9
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-19 21:15:24 -0700

    Update fetch-global-manga.ts

--- src/app/manga/_components/lib/fetch-global-manga.ts
+++ src/app/manga/_components/lib/fetch-global-manga.ts
@@ -1,13 +1,20 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/core";
 import { MangaFolderType, ParentFolderType } from "@/app/dashboard/page";
 
 export default async function fetchGlobalManga(): Promise<MangaFolderType | null> {
-	const result = await invoke("get_global_manga");
-	
-	if (result) {
-		const mangaFolder: MangaFolderType = result as MangaFolderType; 
-		return mangaFolder;
-	} 
+  const result = await invoke("get_global_manga");
+  if (result) {
+    const mangaFolder: MangaFolderType = result as MangaFolderType;
+    return mangaFolder;
+  }
+  return null;
+}
 
-	return null;
+export async function fetchGlobalParent(): Promise<ParentFolderType | null> {
+  const result = await invoke("get_global_parent");
+  if (result) {
+    const parentFolder: ParentFolderType = result as ParentFolderType;
+    return parentFolder;
+  }
+  return null;
 }
