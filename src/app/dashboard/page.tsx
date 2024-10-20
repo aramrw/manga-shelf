5105c60dbdcc6dcba15e699211fb2d782e846953
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-19 21:15:20 -0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { PlusCircleIcon } from "@heroicons/react/16/solid";
-import { open } from "@tauri-apps/api/dialog";
-import { invoke } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/plugin-dialog";
+import { invoke } from "@tauri-apps/api/core";
 import React, { useState, useEffect } from "react";
 import FolderContexMenu from "./_components/parent_folder/folder-context-menu";
-import { FileEntry, readDir } from "@tauri-apps/api/fs";
+import { DirEntry, readDir } from "@tauri-apps/plugin-fs";
 import { useRouter } from "next/navigation";
+import DashboardFooter from "./_components/parent_folder/db-footer";
 
 export interface ParentFolderType {
   id: string;
@@ -15,6 +16,7 @@ export interface ParentFolderType {
   full_path: string;
   as_child: boolean;
   is_expanded: boolean;
+  cover_panel_path: string;
   created_at: string;
   updated_at: string;
 }
@@ -30,7 +32,7 @@ const fileTypes = ["jpg", "jpeg", "png", "gif", "webp"];
 export default function Dashboard() {
   // these can hold both manga folders and child folders
   const [parentFolders, setParentFolders] = useState<ParentFolderType[]>([]);
-  // these are folders hold manga panels not child folders
+  // these are folders that hold manga panels with no child folders
   const [mangaFolders, setMangaFolders] = useState<MangaFolderType[]>([]);
   const router = useRouter();
 
@@ -67,14 +69,10 @@ export default function Dashboard() {
 
     for (const dir of dirs) {
       readDir(dir)
-        .then((dirEntries: FileEntry[]) => {
+        .then((dirEntries: DirEntry[]) => {
           // if the folder directly contains image files its a manga folder
           if (dirEntries && dirEntries[0] && dirEntries?.[0]?.name) {
-            if (
-              fileTypes.some((fileType) =>
-                dirEntries[0]!.name!.toLowerCase().includes(fileType),
-              )
-            ) {
+            if (fileTypes.some((fileType) => dirEntries[0]!.name!.toLowerCase().includes(fileType))) {
               mangaFolderPaths.push(dir);
             } else {
               parentFolderPaths.push(dir);
@@ -98,10 +96,7 @@ export default function Dashboard() {
       isExpanded: false,
     }).then((result: unknown) => {
       if (result) {
-        setParentFolders((prev) => [
-          ...prev,
-          ...(result as ParentFolderType[]),
-        ]);
+        setParentFolders((prev) => [...prev, ...(result as ParentFolderType[])]);
       }
     });
 
@@ -117,14 +112,15 @@ export default function Dashboard() {
   };
 
   const handleMangaClick = (mangaFolderPath: string) => {
-    invoke("set_global_manga", { fullPath: mangaFolderPath }).then(() => {
-      // update all prev manga except clicked on as read before pushing
-      router.push("/manga");
+    invoke("set_global_manga_folder", { fullPath: mangaFolderPath }).then(() => {
+      invoke("set_folder_read", { path: mangaFolderPath }).then((_) => {
+        router.push("/manga");
+      });
     });
   };
 
   return (
-    <main className="w-full h-full">
+    <main className="w-full flex-1 h-full">
       <div className="w-full h-full p-4 lg:px-16 xl:px-36 2xl:px-48">
         <Button
           className="flex flex-row justify-center items-center gap-0.5 shadow-sm
@@ -137,21 +133,14 @@ export default function Dashboard() {
           </span>
         </Button>
         {parentFolders.length > 0 && (
-          <ul className="w-full h-fit grid grid-cols-4 gap-2 mt-4">
+          <ul className="w-fit h-fit grid grid-cols-4 gap-2 mt-4">
             {parentFolders.map((folder, index) => {
-              return (
-                <FolderContexMenu
-                  key={"parent" + index}
-                  folder={folder}
-                  setParentFolders={setParentFolders}
-                  setMangaFolders={setMangaFolders}
-                />
-              );
+              return <FolderContexMenu key={"parent" + index} folder={folder} setParentFolders={setParentFolders} setMangaFolders={setMangaFolders} />;
             })}
           </ul>
         )}
         {mangaFolders.length > 0 && (
-          <ul className="w-full h-fit grid grid-cols-4 gap-2 mt-4">
+          <ul className="w-fit h-full grid grid-cols-4 gap-2 mt-4">
             {mangaFolders.map((folder, index) => (
               <FolderContexMenu
                 key={"manga" + index}
