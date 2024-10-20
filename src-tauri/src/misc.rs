1abf8a1e3e8324ed541fd5e76192ece625a990f5
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-19 21:14:17 -0700

    Update misc.rs

--- src-tauri/src/misc.rs
+++ src-tauri/src/misc.rs
@@ -1,5 +1,12 @@
 use std::process::Command;
 
+use std::sync::LazyLock;
+
+use regex::Regex;
+use sysinfo::System;
+
+pub static NUMBER_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(\d+)\D*$").unwrap());
+
 #[tauri::command]
 pub fn show_in_folder(path: String) {
     #[cfg(target_os = "windows")]
@@ -46,4 +53,42 @@ pub fn show_in_folder(path: String) {
     {
         Command::new("open").args(["-R", &path]).spawn().unwrap();
     }
+}
+
+pub fn close_open_instance() -> bool {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    // Collect and sort processes matching "mpv-shelf"
+    let mut manga_shelf_processes: Vec<_> = sys
+        .processes()
+        .iter()
+        .filter(|(_pid, process)| {
+            process
+                .name()
+                .to_string_lossy()
+                .to_lowercase()
+                .contains("manga")
+        })
+        .collect();
+
+    manga_shelf_processes.sort_by_key(|(pid, _process)| pid.as_u32());
+
+    // Get the current process ID
+    let this_proc_id = std::process::id();
+
+    if manga_shelf_processes.len() > 1 {
+        for (pid, _process) in manga_shelf_processes {
+            // Skip the current process
+            if pid.as_u32() == this_proc_id {
+                continue;
+            }
+            // Attempt to kill the other instances
+            if let Some(proc) = sys.process(*pid) {
+                proc.kill();
+            }
+        }
+    }
+
+    true
 }
