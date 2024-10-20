4a541a11d40e8f22d5d9df80d45482cb2defa87c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-19 21:14:06 -0700

    Update global.rs

--- src-tauri/src/global.rs
+++ src-tauri/src/global.rs
@@ -1,6 +1,6 @@
-use crate::manga::MangaFolder;
+use crate::manga::{MangaFolder, ParentFolder};
 use serde::{Deserialize, Serialize};
-use sqlx::{Row, SqlitePool};
+use sqlx::SqlitePool;
 use tauri::{AppHandle, Manager};
 use tokio::sync::Mutex;
 
@@ -10,7 +10,10 @@ pub struct GlobalError {
 }
 
 #[tauri::command]
-pub async fn set_global_manga(full_path: &str, handle: AppHandle) -> Result<(), GlobalError> {
+pub async fn set_global_manga_folder(
+    full_path: &str,
+    handle: AppHandle,
+) -> Result<(), GlobalError> {
     // This function will be called from the fe
     // and will update the current manga in the global scope
     // so that it can be accessed from anywhere in the app
@@ -18,7 +21,7 @@ pub async fn set_global_manga(full_path: &str, handle: AppHandle) -> Result<(), 
     //println!("Setting global manga: {}", full_path);
 
     let pool = handle.state::<Mutex<SqlitePool>>().lock().await.clone();
-    let current_manga = get_manga_by_path(full_path, &pool).await;
+    let current_manga = get_manga_folder_by_path(full_path, &pool).await.unwrap();
 
     sqlx::query("DELETE FROM global_manga")
         .execute(&pool)
@@ -26,22 +29,23 @@ pub async fn set_global_manga(full_path: &str, handle: AppHandle) -> Result<(), 
         .unwrap();
 
     sqlx::query(
-        "INSERT INTO global_manga 
+        "INSERT INTO global_manga
         (
-            id, 
-            title, 
-            full_path, 
+            id,
+            title,
+            full_path,
             as_child,
             is_expanded,
             time_spent_reading,
             double_panels,
             is_read,
-            created_at, 
+            cover_panel_path,
+            created_at,
             updated_at
-        ) 
+        )
         VALUES
         (
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
         )",
     )
     .bind(current_manga.id)
@@ -52,6 +56,58 @@ pub async fn set_global_manga(full_path: &str, handle: AppHandle) -> Result<(), 
     .bind(current_manga.time_spent_reading)
     .bind(current_manga.double_panels)
     .bind(current_manga.is_read)
+    .bind(current_manga.cover_panel_path)
+    .bind(current_manga.created_at)
+    .bind(current_manga.updated_at)
+    .execute(&pool)
+    .await
+    .unwrap();
+
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn set_global_parent_folder(
+    full_path: &str,
+    handle: AppHandle,
+) -> Result<(), GlobalError> {
+    // This function will be called from the fe
+    // and will update the current manga in the global scope
+    // so that it can be accessed from anywhere in the app
+
+    //println!("Setting global manga: {}", full_path);
+
+    let pool = handle.state::<Mutex<SqlitePool>>().lock().await.clone();
+    let current_manga = get_parent_folder_by_path(full_path, &pool).await.unwrap();
+
+    sqlx::query("DELETE FROM global_parent")
+        .execute(&pool)
+        .await
+        .unwrap();
+
+    sqlx::query(
+        "INSERT INTO global_parent
+        (
+            id,
+            title,
+            full_path,
+            as_child,
+            is_expanded,
+            cover_panel_path,
+            created_at,
+            updated_at
+        )
+        VALUES
+        (
+            ?, ?, ?, ?, ?, ?, ?, ?
+        )",
+    )
+    .bind(current_manga.id)
+    .bind(current_manga.title)
+    .bind(current_manga.full_path)
+    .bind(current_manga.as_child)
+    .bind(current_manga.is_expanded)
+    .bind(current_manga.cover_panel_path)
     .bind(current_manga.created_at)
     .bind(current_manga.updated_at)
     .execute(&pool)
@@ -65,43 +121,44 @@ pub async fn set_global_manga(full_path: &str, handle: AppHandle) -> Result<(), 
 pub async fn get_global_manga(handle: AppHandle) -> Option<MangaFolder> {
     let pool = handle.state::<Mutex<SqlitePool>>().lock().await.clone();
 
-    let result = sqlx::query("SELECT * FROM global_manga")
+    let result: MangaFolder = sqlx::query_as("SELECT * FROM global_manga")
         .fetch_one(&pool)
         .await
         .unwrap();
-
-    Some(MangaFolder {
-        id: result.get("id"),
-        title: result.get("title"),
-        full_path: result.get("full_path"),
-        as_child: result.get("as_child"),
-        is_expanded: result.get("is_expanded"),
-        time_spent_reading: result.get("time_spent_reading"),
-        double_panels: result.get("double_panels"),
-        is_read: result.get("is_read"),
-        created_at: result.get("created_at"),
-        updated_at: result.get("updated_at"),
-    })
+    Some(result)
 }
 
-pub async fn get_manga_by_path(full_path: &str, pool: &SqlitePool) -> MangaFolder {
+#[tauri::command]
+pub async fn get_global_parent(handle: AppHandle) -> Option<ParentFolder> {
+    let pool = handle.state::<Mutex<SqlitePool>>().lock().await.clone();
+
+    let result: ParentFolder = sqlx::query_as("SELECT * FROM global_parent")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+    Some(result)
+}
+
+pub async fn get_manga_folder_by_path(full_path: &str, pool: &SqlitePool) -> Option<MangaFolder> {
     //println!("Getting manga by path: {}", full_path);
-    let result = sqlx::query("SELECT * FROM manga_folder WHERE full_path = ?")
+    match sqlx::query_as("SELECT * FROM manga_folder WHERE full_path = ?")
         .bind(full_path)
         .fetch_one(pool)
         .await
-        .unwrap();
+    {
+        Ok(manga) => Some(manga),
+        Err(_) => None,
+    }
+}
 
-    MangaFolder {
-        id: result.get("id"),
-        title: result.get("title"),
-        full_path: result.get("full_path"),
-        as_child: result.get("as_child"),
-        is_expanded: result.get("is_expanded"),
-        time_spent_reading: result.get("time_spent_reading"),
-        double_panels: result.get("double_panels"),
-        is_read: result.get("is_read"),
-        created_at: result.get("created_at"),
-        updated_at: result.get("updated_at"),
+pub async fn get_parent_folder_by_path(full_path: &str, pool: &SqlitePool) -> Option<ParentFolder> {
+    //println!("Getting manga by path: {}", full_path);
+    match sqlx::query_as("SELECT * FROM parent_folder WHERE full_path = ?")
+        .bind(full_path)
+        .fetch_one(pool)
+        .await
+    {
+        Ok(manga) => Some(manga),
+        Err(_) => None,
     }
 }
