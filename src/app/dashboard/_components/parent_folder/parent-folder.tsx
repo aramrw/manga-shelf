1fa91888e0055039a4567f0301ff9c45e5c156fa
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-19 21:15:18 -0700

    Update parent-folder.tsx

--- src/app/dashboard/_components/parent_folder/parent-folder.tsx
+++ src/app/dashboard/_components/parent_folder/parent-folder.tsx
@@ -1,92 +1,82 @@
 "use client";
-import { FileEntry, readDir } from "@tauri-apps/api/fs";
+import { DirEntry, readDir } from "@tauri-apps/plugin-fs";
 import { MangaFolderType, ParentFolderType } from "../../page";
-import { invoke } from "@tauri-apps/api/tauri";
+import { convertFileSrc, invoke } from "@tauri-apps/api/core";
 import { useCallback, useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 // import {
 //   HoverCard,
 //   HoverCardContent,
 //   HoverCardTrigger,
 // } from "@/components/ui/hover-card";
 import FolderContextMenu from "./folder-context-menu";
+import Image from "next/image";
+import { MangaPanelType } from "@/app/manga/page";
 
 const fileTypes = ["jpg", "jpeg", "png", "gif", "webp"];
 
-export default function ParentFolder({
-  parentFolder,
-}: {
-  parentFolder: ParentFolderType;
-}) {
+export default function ParentFolder({ parentFolder }: { parentFolder: ParentFolderType }) {
   // these can hold both manga folders and child folders
   const [parentFolders, setParentFolders] = useState<ParentFolderType[]>([]);
-  // these only hold manga panels
+  // these only hold manga image panels
   const [mangaFolders, setMangaFolders] = useState<MangaFolderType[]>([]);
-  const [isExpanded, setIsExpanded] = useState<boolean>(
-    parentFolder.is_expanded,
-  );
+  const [isExpanded, setIsExpanded] = useState<boolean>(parentFolder.is_expanded);
   const router = useRouter();
+  const pathname = usePathname();
 
-  const invokeAddFolders = useCallback(
-    (parentDirs: string[], mangaDirs: string[]) => {
-      invoke("update_parent_folders", {
-        dirPaths: JSON.stringify(parentDirs),
-        asChild: true,
-        isExpanded: false,
-      }).then((result: unknown) => {
-        if (result) {
-          const currentParentFolders: ParentFolderType[] =
-            result as ParentFolderType[];
-          setParentFolders(currentParentFolders);
-        }
-      });
+  const invokeAddFolders = useCallback((parentDirs: string[], mangaDirs: string[]) => {
+    invoke("update_parent_folders", {
+      dirPaths: JSON.stringify(parentDirs),
+      asChild: true,
+      isExpanded: false,
+    }).then((result: unknown) => {
+      if (result) {
+        const currentParentFolders: ParentFolderType[] = result as ParentFolderType[];
+        setParentFolders(currentParentFolders);
+      }
+    });
 
-      invoke("update_manga_folders", {
-        dirPaths: JSON.stringify(mangaDirs),
-        asChild: true,
-        isExpanded: false,
-      }).then((result: unknown) => {
-        if (result) {
-          const currentMangaFolders: MangaFolderType[] =
-            result as MangaFolderType[];
-          setMangaFolders(currentMangaFolders);
-        }
-      });
-    },
-    [],
-  );
+    invoke("update_manga_folders", {
+      dirPaths: JSON.stringify(mangaDirs),
+      asChild: true,
+      isExpanded: false,
+    }).then((result: unknown) => {
+      if (result) {
+        const currentMangaFolders: MangaFolderType[] = result as MangaFolderType[];
+        setMangaFolders(currentMangaFolders);
+      }
+    });
+  }, []);
 
   const readDirSort = useCallback(
-    (dir: string) => {
+    async (dir: string) => {
       const parentFolderPaths: string[] = [];
       const mangaFolderPaths: string[] = [];
 
-      readDir(dir, { recursive: true })
-        .then((dirEntries: FileEntry[]) => {
-          for (const entry of dirEntries) {
-            if (
-              entry.children &&
-              entry.children[0] &&
-              entry.children[0].name &&
-              fileTypes.some(
-                (fileType) =>
-                  entry.children &&
-                  entry.children[0].name &&
-                  entry.children[0].name.toLowerCase().includes(fileType),
-              )
-            ) {
-              mangaFolderPaths.push(entry.path);
-            } else {
-              parentFolderPaths.push(entry.path);
-            }
+      const dirEntries = await readDir(dir);
+
+      const folderPromises = dirEntries.map(async (entry) => {
+        let entryPath = `${dir}\\${entry.name}`;
+        if (entry.isDirectory) {
+          const subEntries = await readDir(entryPath);
+          let firstSubEntry = subEntries[0];
+          let firstSubEntryFileType = firstSubEntry.name.split(".")[1];
+          if (firstSubEntry && firstSubEntry.name && fileTypes.includes(firstSubEntryFileType)) {
+            mangaFolderPaths.push(entryPath);
+          } else {
+            parentFolderPaths.push(entryPath);
           }
-        })
-        .then(() => {
-          if (parentFolderPaths.length > 0 || mangaFolderPaths.length > 0) {
-            invokeAddFolders(parentFolderPaths, mangaFolderPaths);
-          }
-        });
+        }
+      });
+
+      await Promise.all(folderPromises);
+
+      if (parentFolderPaths.length > 0 || mangaFolderPaths.length > 0) {
+        parentFolderPaths.sort((a, b) => a.localeCompare(b));
+        mangaFolderPaths.sort((a, b) => a.localeCompare(b));
+        invokeAddFolders(parentFolderPaths, mangaFolderPaths);
+      }
     },
     [invokeAddFolders],
   );
@@ -95,35 +85,12 @@ export default function ParentFolder({
     readDirSort(parentFolder.full_path);
   }, [parentFolder.full_path, readDirSort]);
 
-  const handleMangaClick = (mangaFolderPath: string) => {
-    console.log(mangaFolders);
-    const index = mangaFolders.findIndex(
-      (fold) => fold.full_path === mangaFolderPath,
-    );
-    for (let i = 0; i < mangaFolders.length; i++) {
-      //console.log(`setting ${mangaFolders[i].id} as read`);
-      if (i < index) {
-        invoke("set_folder_read", { path: mangaFolders[i]?.full_path });
-      } else {
-        invoke("set_folder_unread", { path: mangaFolders[i]?.full_path });
-      }
-    }
-    console.log("pushing to next manga");
-    invoke("set_global_manga", { fullPath: mangaFolderPath }).then(() => {
-      router.push("/manga");
-    });
-  };
-
   return (
     <ul
       className={cn(
         `
-					h-fit
-					w-full 
-					p-1 
-					bg-primary
-					flex 
-					flex-col
+					flex
+					flex-row
 					justify-center
 					items-center
 					text-center
@@ -131,18 +98,18 @@ export default function ParentFolder({
 					cursor-pointer
 					will-change-transform
 					shadow-sm
+					bg-primary transition-transform duration-100 ease-in-out
 				`,
-        !isExpanded &&
-        !parentFolder.as_child &&
-        "hover:scale-[1.005] transition-transform duration-100 ease-in-out",
-        parentFolder.as_child && "p-0 rounded-none text-xs",
+        !isExpanded && !parentFolder.as_child && "hover:scale-[1.005]",
+        !parentFolder.as_child && "h-36 w-28 md:h-52 md:w-36 lg:h-64 lg:w-48 xl:h-72 xl:w-52",
+        parentFolder.as_child && "max-w-28 h-36 text-xs hover:translate-y-[1px]",
       )}
     >
       <li
         tabIndex={0}
         className={cn(
-          "p-1 mb-0.5 w-full h-full flex flex-col justify-center items-center bg-secondary focus-visible:outline focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-muted-foreground focus-visible:ring-opacity-50 ring-inset",
-          parentFolder.as_child && "bg-accent-foreground",
+          "mb-0.5 w-full h-full flex flex-col justify-center items-center relative focus-visible:outline focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-muted-foreground focus-visible:ring-opacity-50 ring-inset",
+          parentFolder.as_child && "bg-transparent rounded-sm",
         )}
         onKeyDown={(e) => {
           if (e.key === "Enter") {
@@ -156,46 +123,43 @@ export default function ParentFolder({
           }
         }}
         onClick={() => {
-          invoke("update_parent_folders", {
-            dirPaths: JSON.stringify([parentFolder.full_path]),
-            asChild: parentFolder.as_child,
-            isExpanded: !isExpanded,
+          invoke("set_global_parent_folder", { fullPath: parentFolder.full_path }).then(() => {
+            if (pathname === "/manga-chapters") {
+            }
+            router.push("/manga-chapters");
           });
-
-          setIsExpanded(!isExpanded);
         }}
+        style={{ position: "relative" }} // Position relative to apply pseudo-element background
       >
-        <h1 className="font-bold w-full overflow-hidden text-nowrap">
-          {parentFolder.title}
-        </h1>
+        {/* Pseudo-element for blurred background */}
+        <style jsx>{`
+          li::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-image: url(${convertFileSrc(parentFolder.cover_panel_path)});
+            filter: blur(2px); /* Adjust the blur radius as needed */
+            z-index: 0; /* Place it behind the image */
+            background-size: cover;
+            background-position: center;
+          }
+        `}</style>
+        {parentFolder.cover_panel_path ? (
+          <Image
+            alt={parentFolder.title}
+            className="object-contain object-left w-full h-full relative z-10" // Ensure image stays on top
+            src={convertFileSrc(parentFolder.cover_panel_path)}
+            fill
+            priority
+            quality={100}
+          />
+        ) : (
+          <span>loading...</span>
+        )}
       </li>
-      {isExpanded && (
-        <div className="max-h-60 w-full overflow-auto pr-0.5">
-          {parentFolders.length > 0 && (
-            <li className="w-full h-fit flex flex-col justify-center items-center bg-secondary">
-              {parentFolders.map((folder, index) => (
-                <ParentFolder
-                  key={`${folder.id}-${index}`}
-                  parentFolder={folder}
-                />
-              ))}
-            </li>
-          )}
-          <li className="w-full h-fit flex flex-col justify-center items-center bg-secondary">
-            {mangaFolders.map((folder, index) => (
-              <FolderContextMenu
-                key={`manga${index}`}
-                folder={folder}
-                setMangaFolders={setMangaFolders}
-                setParentFolders={setParentFolders}
-                handleMangaClick={handleMangaClick}
-                isMangaFolder
-                asChild
-              />
-            ))}
-          </li>
-        </div>
-      )}
     </ul>
   );
 }
